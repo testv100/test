1605f50c23492cfb18103a2180515824ed8010af
testv100 <test_version_1.0.0@proton.me>
2026-01-18 17:56:49 +0300

    Delete PythonProject2.1 directory

--- PythonProject2.1/analytics/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class ResultsUploadForm(forms.Form):
-    file = forms.FileField()
-    preview = forms.BooleanField(required=False, initial=True)
-
-class TeacherUserLinkForm(forms.Form):
-    user_id = forms.IntegerField()
-    teacher_id = forms.IntegerField()
